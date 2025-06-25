e93cfde3c8f75d40e666504abd0b11bdf0f6d1dd
r0naldy <ronaldyitolopez@gmail.com>
2025-06-25 17:42:06 -0500

    Corrige sintaxis HCL, elimina ; en bloques principals

--- main.tf
+++ main.tf
@@ -12,14 +12,19 @@ data "aws_s3_bucket" "data_bucket" {
 data "aws_iam_policy_document" "lambda_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals { type = "Service"; identifiers = ["lambda.amazonaws.com"] }
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
   }
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda-s3-role"
+  name               = "lambda-s3-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
 }
+
 
 data "aws_iam_policy_document" "lambda_s3_access" {
   statement {
